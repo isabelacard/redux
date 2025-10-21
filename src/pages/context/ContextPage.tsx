f30b63d6abff632fd76f90e68215833339e5af4a
Isabela Cardona <1111481156@u.icesi.edu.co>
2025-10-21 12:02:23 -0500

    changes

--- src/pages/context/ContextPage.tsx
+++ src/pages/context/ContextPage.tsx
@@ -1,14 +1,19 @@
-import { createContext, useState } from "react";
+import React, { createContext, useState } from "react";
 
 import ComponentA from "./components/ComponentA";
 
-export const CounterContext = createContext(null);
+export type ContextType = {
+    counter: number;
+    setCounter: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const CounterContext = createContext<ContextType | null>(null);
 
 export default function ContextPage() {
     console.info("Componente ContextPage renderizandose");
     const [counter, setCounter] = useState(0);
     return (
-        <div className="card shadow-2xl flex flex-col justify-start items-center p-5 min-w-screen">
+        <div className="card shadow-2xl flex flex-col justify-start items-center p-5">
             <h1>Context page!</h1>
             <p>{counter}</p>
             <button className="btn btn-primary" onClick={() => setCounter((prev) => prev + 1)}>
--- src/pages/context/components/ComponentC.tsx
+++ src/pages/context/components/ComponentC.tsx
@@ -1,15 +1,18 @@
 import { useContext } from "react";
 
-import { CounterContext } from "../ContextPage";
+import { type ContextType, CounterContext } from "../ContextPage";
 
 export default function ComponentC() {
-    const { counter, setCounter } = useContext(CounterContext);
+    const ctx = useContext<ContextType | null>(CounterContext);
+    if (!ctx) {
+        throw new Error("Contexto no provisto");
+    }
     console.info("Componente C renderizandose");
     return (
         <div className="p-6 w-128 border-4 rounded-4xl min-w-max">
             <h1>Mi componente C</h1>
-            <p>{counter}</p>
-            <button className="btn btn-secondary" onClick={() => setCounter((prev) => prev + 1)}>
+            <p>{ctx.counter}</p>
+            <button className="btn btn-secondary" onClick={() => ctx.setCounter((prev) => prev + 1)}>
                 Aumentar
             </button>
         </div>
